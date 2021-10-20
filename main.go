09f4307108728125961d46eb53376ec2936f132a
Dmitrii Krikunov <krikunov@algont.ru>
2021-10-20 16:00:48 +0300

    tests

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+)
 
 func main() {
-	fmt.Print("Run exameple \n")
+	fmt.Print("Run example \n")
+
+	scores := make([]int, 100)
+	for i := 0; i < 100; i++ {
+		scores[i] = int(rand.Int31n(1000))
+	}
+	sort.Ints(scores)
+
+	worst := make([]int, 10)
+	copy(worst, scores[0:5])
+	worst = append(worst, scores[1])
+	fmt.Println(worst)
+	fmt.Println(scores)
 }
